bef11779e7e0e272483be74c9ccc72d8e39e374c
Sean Russell <emailseanrussell@gmail.com>
2021-07-18 20:04:38 -0700

    formatting and finalizing for submission

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,34 +1,34 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// a function that returns a license badge based on which license is passed in
+// NOTE: if there is no license, an empty string is returned
 function renderLicenseBadge(license) {
   if (license === "None") {
     return 'License and badge unknown, no entry'
   } else if (license === "MIT") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" 
-    
- } else if (license === "Apache") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+
+  } else if (license === "Apache") {
     return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
   } else if (license === "GNU") {
     return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
   }
 }
-    
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+
+// a function that returns the license link
+// NOTE: if there is no license, an empty string is returned
 function renderLicenseLink(license) {
   if (license === "None") {
     return ''
   } else if (license === "MIT") {
     return "https://opensource.org/licenses/MIT"
- } else if (license === "Apache") {
+  } else if (license === "Apache") {
     return "https://opensource.org/licenses/Apache-2.0"
   } else if (license === "GNU") {
     return "https://www.gnu.org/licenses/gpl-3.0"
   }
 }
-  
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+
+// a function that returns the license section of README
+// NOTE: if there is no license, an empty string is returned
 function renderLicenseSection(license) {
   if (license === "None") {
     return 'License and badge unknown, no entry'
@@ -36,27 +36,27 @@ function renderLicenseSection(license) {
     return ` 
     Copyright (C) [2021] [Sean Russell]
 
-      Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
     The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
     
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     `
- } else if (license === "Apache") {
+  } else if (license === "Apache") {
     return `
     Copyright (C) [2021] [Sean Russell]
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.`
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.`
   } else if (license === "GNU") {
     return ` 
     Copyright (C) [2021] [Sean Russell]
@@ -73,10 +73,10 @@ limitations under the License.`
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.`
-  } 
+  }
 }
 
-// TODO: Create a function to generate markdown for README
+// a function to generate markdown for the README
 function generateMarkdown(data) {
   return `# ${data.projectTitle}
   ${renderLicenseBadge(data.licenseList)}
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const util = require('util');
 const generateMarkdown = require('./generateMarkdown');
-// const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create an array of questions for user input
+// an array of questions for user input
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -24,53 +22,51 @@ const promptUser = () => {
       message: "Please enter any installation instructions:",
     },
     {
-        type: 'input',
-        name: 'usageInfo',
-        message: "Please enter any usage information:",
+      type: 'input',
+      name: 'usageInfo',
+      message: "Please enter any usage information:",
     },
     {
-        type: 'input',
-        name: 'contribGuide',
-        message: "Please enter any contribution guidelines:",
+      type: 'input',
+      name: 'contribGuide',
+      message: "Please enter any contribution guidelines:",
     },
     {
-        type: 'input',
-        name: 'testInstruct',
-        message: "Please enter any test instructions:",
+      type: 'input',
+      name: 'testInstruct',
+      message: "Please enter any test instructions:",
     },
     {
-        type: 'input',
-        name: 'gitHubId',
-        message: 'Please enter your GitHub ID:',
+      type: 'input',
+      name: 'gitHubId',
+      message: 'Please enter your GitHub ID:',
     },
     {
-        type: 'input',
-        name:'contactEmail',
-        message: 'Please enter your contact email:',
+      type: 'input',
+      name: 'contactEmail',
+      message: 'Please enter your contact email:',
     },
     {
-        type: 'list',
-        name: 'licenseList',
-        message: "Please select from this list of licenses:",
-        choices: ['None', 'MIT','GNU','Apache']
+      type: 'list',
+      name: 'licenseList',
+      message: "Please select from this list of licenses:",
+      choices: ['None', 'MIT', 'GNU', 'Apache']
     },
-]);
+  ]);
 };
 
-// TODO: Create a function to write README file
+// a function to write the README file
 function writeToFile(fileName, data) {
-  fs.appendFile(`${fileName}.md`, data, 
-  (err) => err ? console.error(err) : console.log(`We have created ${fileName}.md successfully.`))
+  fs.appendFile(`${fileName}.md`, data,
+    (err) => err ? console.error(err) : console.log(`We have created ${fileName}.md successfully.`))
 }
-// ${answers.licenseList}
 
-
-// TODO: Create a function to initialize app
+// a function to initialize the app
 function init() {
   promptUser()
-  .then((answers) => {
-   writeToFile(answers.projectTitle, generateMarkdown(answers));
-  })
+    .then((answers) => {
+      writeToFile(answers.projectTitle, generateMarkdown(answers));
+    })
 }
 
 // Function call to initialize app
